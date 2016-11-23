42af1bb8aa95f91687062de80765201ef464b259
binggo <binggozhan@163.com>
2016-11-23 10:27:46 +0800

    update

--- src/main/java/org/binggo/testspring/TestSpring.java
+++ src/main/java/org/binggo/testspring/TestSpring.java
@@ -1,7 +1,8 @@
 package org.binggo.testspring;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 
 public class TestSpring 
 {
@@ -9,8 +10,9 @@ public class TestSpring
 	
 	public static void main( String[] args )
     {
-		ApplicationContext context = new ClassPathXmlApplicationContext(XML_FILE);
-        Person person = (Person) context.getBean("person");
+		//ApplicationContext context = new ClassPathXmlApplicationContext(XML_FILE);
+		BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource(XML_FILE));
+        Person person = (Person) beanFactory.getBean("person");
         System.out.println(String.format("Person's name: %s", person.getName()));
     }
 }
